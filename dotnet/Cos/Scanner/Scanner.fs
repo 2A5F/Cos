ddeda3ee9b4dc1aeb50ffe526fc968a25a8ba7ea
2A5F <u2a5f@outlook.com>
2021-02-15 13:56:40 +0800

    num char

--- dotnet/Cos/Scanner/Scanner.fs
+++ dotnet/Cos/Scanner/Scanner.fs
@@ -2,6 +2,7 @@ module rec Volight.Cos.Parser.Scanner
 
 open System
 open System.Collections.Generic
+open System.Globalization
 open Volight.Cos.SrcPos
 open Volight.Cos.Utils
 open Volight.Cos.Utils.Utils
@@ -112,33 +113,289 @@ let internal at (ctx: Ctx) (code: Code) =
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// todo num
+let inline internal isNumFirst c = (c >= '0' && c <= '9')
+let inline internal isNumBody c = (c = '_') || (c >= '0' && c <= '9')
+let inline internal isNumHexBody c = (c = '_') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') || (c >= '0' && c <= '9')
+let inline internal isNumBinaryBody c = match c with '_' | '0' | '1'  -> true | _ -> false
+let inline internal isNumBinaryIllegal c = (c >= '2' && c <= '9')
+
+let inline internal numFinish (ctx: Ctx) (code: Code) prefix suffxi dot f e i illegal =
+    let range = code.CodeRange(f, e)
+    let loc = ctx.Loc range
+    let fuloc = Loc.OfMP prefix suffxi loc
+    if illegal then ctx.errs.Add(IllegalNumber fuloc)
+    else 
+        let str = ctx.SubStr range
+        let tk = Token.New(str, loc)
+        let num = TNum(tk, prefix, suffxi, dot, fuloc)
+        ctx.tokens.Add(Num num)
+    Just <| code.CodeRangeFrom i
+
+let internal numSuffix (ctx: Ctx) (code: Code) prefix dot f e i illegal =
+    match code.[i] with
+    | Just c when isIdBody c -> numSuffix ctx code prefix dot f e (i + 1) illegal
+    | _ ->
+        let range = code.CodeRange(e, i)
+        let loc = ctx.Loc range
+        let str = ctx.SubStr range
+        let tk = Token.New(str, loc)
+        numFinish ctx code prefix (Just tk) dot f e i illegal
+
+let internal numBodyBinary (ctx: Ctx) (code: Code) prefix f i first illegal =
+    match code.[i] with
+    | Just '.' -> numBodyBinary ctx code prefix f (i + 1) false true
+    | Just c when isNumBinaryBody c -> numBodyBinary ctx code prefix f (i + 1) false illegal
+    | Just c when isNumBinaryIllegal c -> numBodyBinary ctx code prefix f (i + 1) false true
+    | Just c when isIdFirst c -> numSuffix ctx code (Just prefix) false f i (i + 1) illegal
+    | _ -> 
+    if first then ctx.errs.Add(UnknownSymbol ctx.pos.[code.RawIndex i])
+    numFinish ctx code (Just prefix) Nil false f i i illegal
+
+let internal numBodyHex (ctx: Ctx) (code: Code) prefix f i first illegal =
+    match code.[i] with
+    | Just '.' -> numBodyHex ctx code prefix f (i + 1) false true
+    | Just c when isNumHexBody c -> numBodyHex ctx code prefix f (i + 1) false illegal
+    | Just c when isIdFirst c -> numSuffix ctx code (Just prefix) false f i (i + 1) illegal
+    | _ -> 
+    if first then ()
+    numFinish ctx code (Just prefix) Nil false f i i illegal
+
+let internal numBodyDecimal (ctx: Ctx) (code: Code) i =
+    match code.[i] with
+    | Just c when isNumBody c -> numBodyDecimal ctx code (i + 1)
+    | Just c when isIdFirst c -> numSuffix ctx code Nil true 0 i (i + 1) false
+    | _ -> numFinish ctx code Nil Nil true 0 i i false
+
+let internal numBody (ctx: Ctx) (code: Code) i =
+    match code.[i] with
+    | Just '.' -> numBodyDecimal ctx code (i + 1)
+    | Just c when isNumBody c -> numBody ctx code (i + 1)
+    | Just c when isIdFirst c -> numSuffix ctx code Nil false 0 i (i + 1) false
+    | _ -> numFinish ctx code Nil Nil false 0 i i false
+
+let internal numZeroFirst (ctx: Ctx) (code: Code) =
+    match code.[1] with
+    | Just ('x' | 'X') -> 
+        let range = code.CodeRangeTo 2
+        let loc = ctx.Loc range
+        let str = ctx.SubStr range
+        let tk = Token.New(str, loc)
+        numBodyHex ctx code tk 2 2 true false
+    | Just ('b' | 'B') -> 
+        let range = code.CodeRangeTo 2
+        let loc = ctx.Loc range
+        let str = ctx.SubStr range
+        let tk = Token.New(str, loc)
+        numBodyBinary ctx code tk 2 2 true false
+    | Just '.' -> numBodyDecimal ctx code 2
+    | Just c when isNumBody c -> numBody ctx code 2
+    | Just c when isIdFirst c -> numSuffix ctx code Nil false 0 2 2 false
+    | _ -> numFinish ctx code Nil Nil false 0 1 1 false
+
+let internal num (ctx: Ctx) (code: Code) =
+    match code.First with
+    | Just '0' -> numZeroFirst ctx code
+    | Just c when isNumFirst c -> numBody ctx code 1
+    | _ -> Nil
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let inline internal isHex c = (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') || (c >= '0' && c <= '9')
+
+let internal escapeDigit (ctx: Ctx) (code: Code) i =
+    match code.[i + 1] with
+    | Just c when Char.IsDigit(c) ->
+        match code.[i + 2] with
+        | Just c when Char.IsDigit(c) ->
+            let span = code.[i..i + 3].span
+            let mutable c = 0uy
+            let s = Byte.TryParse(span, &c)
+            if not s then
+                let loc = ctx.Loc <| code.CodeRange(i, i + 3)
+                ctx.errs.Add(IllegalEscape(loc, Digit))
+                struct (i + 3, Nil)
+            else
+                struct (i + 3, Just <| Operators.char c)
+        | _ ->
+            let loc = ctx.Loc <| code.CodeRange(i, i + 2)
+            ctx.errs.Add(IllegalEscape(loc, Digit))
+            struct (i + 2, Nil)
+    | _ ->
+        let loc = ctx.Loc <| code.CodeRange(i, i + 1)
+        ctx.errs.Add(IllegalEscape(loc, Digit))
+        struct (i + 1, Nil)
+
+let internal escapeHex (ctx: Ctx) (code: Code) i =
+    match code.[i + 1] with
+    | Just c when isHex c ->
+        match code.[i + 2] with
+        | Just c when isHex c ->
+            let span = code.[i + 1 .. i + 3].span
+            let mutable c = 0uy
+            let s = Byte.TryParse(span, NumberStyles.HexNumber, null, &c)
+            if not s then
+                let loc = ctx.Loc <| code.CodeRange(i, i + 3)
+                ctx.errs.Add(IllegalEscape(loc, Hex))
+                struct (i + 3, Nil)
+            else
+                struct (i + 3, Just <| Operators.char c)
+        | _ ->
+            let loc = ctx.Loc <| code.CodeRange(i, i + 1)
+            ctx.errs.Add(IllegalEscape(loc, Hex))
+            struct (i + 2, Nil)
+    | _ ->
+        let loc = ctx.Loc <| code.CodeRange(i, i)
+        ctx.errs.Add(IllegalEscape(loc, Hex))
+        struct (i + 1, Nil)
+
+let internal escapeUnicode (ctx: Ctx) (code: Code) f i =
+    match code.[i] with
+    | Just c when isHex c ->
+        if i - f < 4 then escapeUnicode ctx code f (i + 1) else
+        let span = code.[f + 1 .. i + 1].span
+        let mutable c = 0us
+        let s = UInt16.TryParse(span, NumberStyles.HexNumber, null, &c)
+        if not s then
+            let loc = ctx.Loc <| code.CodeRange(f, i + 1)
+            ctx.errs.Add(IllegalEscape(loc, Unicode))
+            struct (i + 1, Nil)
+        else
+            struct (i + 1, Just <| Operators.char c)
+    | _ ->
+        let loc = ctx.Loc <| code.CodeRange(f, i)
+        ctx.errs.Add(IllegalEscape(loc, Unicode))
+        struct (i + 1, Nil)
+
+let internal escapeBigUnicode (ctx: Ctx) (code: Code) f i =
+    match code.[i] with
+    | Just c when isHex c ->
+        if i - f < 6 then escapeBigUnicode ctx code f (i + 1) else
+        let span = code.[f + 1 .. i + 1].span
+        let mutable c = 0
+        let s = Int32.TryParse(span, NumberStyles.HexNumber, null, &c)
+        if not s then
+            let loc = ctx.Loc <| code.CodeRange(f, i)
+            ctx.errs.Add(IllegalEscape(loc, Unicode))
+            struct (i + 1, Nil)
+        else
+            struct (i + 1, Just <| Char.ConvertFromUtf32 c)
+    | _ ->
+        let loc = ctx.Loc <| code.CodeRange(f, i)
+        ctx.errs.Add(IllegalEscape(loc, Unicode))
+        struct (i + 1, Nil)
+
+let internal charEscape (ctx: Ctx) (code: Code) i =
+    match code.[i] with
+    | Just '0' -> struct (i + 1, Just '\000')
+    | Just 'a' -> struct (i + 1, Just '\a')
+    | Just 'b' -> struct (i + 1, Just '\b')
+    | Just 'f' -> struct (i + 1, Just '\f')
+    | Just 'n' -> struct (i + 1, Just '\n')
+    | Just 'r' -> struct (i + 1, Just '\r')
+    | Just 't' -> struct (i + 1, Just '\t')
+    | Just 'v' -> struct (i + 1, Just '\v')
+    | Just '\\' -> struct (i + 1, Just '\\')
+    | Just ''' -> struct (i + 1, Just ''')
+    | Just '"' -> struct (i + 1, Just '"')
+    | Just c when Char.IsDigit(c) -> escapeDigit ctx code i
+    | Just 'x' -> escapeHex ctx code i
+    | Just 'u' -> escapeUnicode ctx code i (i + 1)
+    | _ -> 
+        let pos = ctx.pos.[code.RawIndex i]
+        ctx.errs.Add(UnknownEscape(pos))
+        struct (i + 1, Nil)
+
+let internal strEscape (ctx: Ctx) (code: Code) i =
+    match code.[i] with
+    | Just '0' -> struct (i + 1, Just "\000")
+    | Just 'a' -> struct (i + 1, Just "\a")
+    | Just 'b' -> struct (i + 1, Just "\b")
+    | Just 'f' -> struct (i + 1, Just "\f")
+    | Just 'n' -> struct (i + 1, Just "\n")
+    | Just 'r' -> struct (i + 1, Just "\r")
+    | Just 't' -> struct (i + 1, Just "\t")
+    | Just 'v' -> struct (i + 1, Just "\v")
+    | Just '\\' -> struct (i + 1, Just "\\")
+    | Just ''' -> struct (i + 1, Just "'")
+    | Just '"' -> struct (i + 1, Just "\"")
+    | Just '$' -> struct (i + 1, Just "$")
+    | Just c when Char.IsDigit(c) -> 
+        match escapeDigit ctx code i with
+        | (i, Just c) -> (i, Just <| c.ToString())
+        | (i, Nil) -> (i, Nil)
+    | Just 'x' -> 
+        match escapeHex ctx code i with
+        | (i, Just c) -> (i, Just <| c.ToString())
+        | (i, Nil) -> (i, Nil)
+    | Just 'u' -> 
+        match escapeUnicode ctx code i (i + 1) with
+        | (i, Just c) -> (i, Just <| c.ToString())
+        | (i, Nil) -> (i, Nil)
+    | Just 'U' -> escapeBigUnicode ctx code i (i + 1)
+    | _ -> 
+        let pos = ctx.pos.[code.RawIndex i]
+        ctx.errs.Add(UnknownEscape(pos))
+        struct (i + 1, Nil)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let internal charEnd (ctx: Ctx) (code: Code) q i c raw =
+    match code.[i] with 
+    | Just e when e = q -> 
+        let range = code.CodeRangeTo (i + 1)
+        let loc = ctx.Loc range
+        let c = TChar.New(c, raw, loc)
+        ctx.tokens.Add(Tokens.Char c)
+        Just <| code.CodeRangeFrom (i + 1)
+    | _ -> 
+        let pos = ctx.pos.[code.RawIndex i]
+        ctx.errs.Add(CharNotClosed(pos))
+        Just <| code.CodeRangeFrom (i + 1) // or Nil
+
+let internal char (ctx: Ctx) (code: Code) =
+    match code.First with
+    | Just 'c' ->
+        match code.[1] with
+        | Just (q & (''' | '"')) -> 
+            match code.[2] with
+            | Just '\\' -> 
+                match charEscape ctx code 3 with
+                | (i, Just c) -> 
+                    let raw = ctx.SubStr <| code.CodeRange(2, i)
+                    charEnd ctx code q i c (Just raw) 
+                | (i, Nil) -> Just <| code.CodeRangeFrom i // or Nil
+            | Just c -> charEnd ctx code q 3 c Nil 
+            | Nil -> 
+                let pos = ctx.pos.[code.offset]
+                raise <| ScannerException(UnexpectedEof(pos))
+        | _ -> Nil
+    | _ -> Nil
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let inline internal blockItem (ctx: Ctx) (code: Code) =
     match space ctx code with
     | Nil ->
-        match id ctx code with 
-        | Nil -> 
-            match split ctx code with 
+        match char ctx code with 
+        | Nil ->
+            match id ctx code with 
             | Nil -> 
-                match comma ctx code with
+                match split ctx code with 
                 | Nil -> 
-                    match block ctx code with
-                    | Nil ->
-                        match oper ctx code with
+                    match comma ctx code with
+                    | Nil -> 
+                        match block ctx code with
                         | Nil ->
-                            match at ctx code with
-                            // | Nil ->
-                            //     match num ctx code with
-                            //     | Nil ->
-                            //         match char ctx code with
-                            //         | Nil ->
-                            //             match str ctx code with
-                            //             | r -> r
-                            //         | r -> r
-                            //     | r -> r
+                            match oper ctx code with
+                            | Nil ->
+                                match at ctx code with
+                                | Nil ->
+                                    match num ctx code with
+                                    // | Nil ->
+                                    //     match str ctx code with
+                                    //     | r -> r
+                                    | r -> r
+                                | r -> r
                             | r -> r
                         | r -> r
                     | r -> r
@@ -186,7 +443,7 @@ let internal block (ctx: Ctx) (code: Code) =
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// todo str char
+// todo str
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
