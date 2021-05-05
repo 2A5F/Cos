c057469947a53fccd97d6d039fbf0f76c6cc28c3
2A5F <u2a5f@outlook.com>
2021-05-05 21:47:21 +0800

    change keyword

--- dotnet/Cos/Scanner/KeyWord.fs
+++ dotnet/Cos/Scanner/KeyWord.fs
@@ -5,17 +5,23 @@ open System.Linq
 open System.Collections.Generic
 open Volight.Cos.Utils
 open Volight.Cos.Utils.SubStrEx
+open Microsoft.FSharp.Reflection
 
+[<Struct>]
 type KeyWord =
-| Var = 0uy | Let = 1uy | If = 2uy | Else = 3uy | Do = 4uy | Case = 5uy | Of = 6uy | With = 7uy | For = 8uy | While = 9uy | Break = 10uy 
-| Continue = 11uy | Return = 12uy | Try = 13uy | Throw = 14uy | Catch = 15uy | Finally = 16uy | Fn = 17uy | Def = 18uy | Data = 19uy | Kind = 20uy | Enum = 21uy
-| TMe = 22uy | Me = 23uy | Where = 24uy | Module = 25uy | Import = 26uy | Export = 27uy | Is = 28uy | As = 29uy | Const = 30uy | Static = 31uy
-| Co = 32uy | Inline = 33uy | Throws = 34uy | Tail = 35uy | Public = 36uy | Private = 37uy | Internal = 38uy | Protected = 39uy | Goto = 40uy | Need = 41uy
-| In = 42uy | Underscore = 43uy | False = 44uy | True = 45uy | Mut = 46uy 
-/// try!
-| TryE = 47uy 
-/// try?
-| TryQ = 48uy
+| Underscore
+| Let | Mut | Alt
+| If | Else | Case | Of | Is | As
+| Do | For | While | With | In
+| Break | Continue | Return | Goto
+| Try | Throw | Catch | Throws | Delay
+| Fun | Co | Inline | NoInline | RefCtx | Tail
+| Class | Trait | Enum | Type | Def
+| TSelf | Self | Where | Static | New
+| Public | Private | Internal | Protected
+| Module | Import | Export
+| True | False
+| TryE | TryQ
 
 module KeyWords =
     [<AbstractClass; Sealed>]
@@ -23,11 +29,11 @@ module KeyWords =
         static let keyWord2Strs i = 
             struct(i, 
                 match i with 
-                | KeyWord.TMe -> "Me" 
-                | KeyWord.Underscore -> "_" 
-                | KeyWord.TryE -> "try!" 
-                | KeyWord.TryQ -> "try?" 
-                | _ -> Enum.GetName(i).ToLower() 
+                | TSelf -> "Self" 
+                | Underscore -> "_" 
+                | TryE -> "try!"
+                | TryQ -> "try?"
+                | _ -> match FSharpValue.GetUnionFields(i, typeof<KeyWord>) with | case, _ -> case.Name.ToLower() 
             ) 
         static let _strs: struct (KeyWord * string) [] = Enum.GetValues(typeof<KeyWord>).Cast<KeyWord>().Select(keyWord2Strs).ToArray()
         static let _enumToStr: Dictionary<KeyWord, string> = Dictionary()
@@ -53,8 +59,8 @@ module KeyWords =
 
     let idAllowed k =
         match k with
-        | KeyWord.Co | KeyWord.Inline | KeyWord.Tail | KeyWord.Throws
-        | KeyWord.Data | KeyWord.Kind | KeyWord.Enum | KeyWord.Need
-        | KeyWord.Underscore | KeyWord.Me | KeyWord.TMe
+        | Co | Inline | NoInline | RefCtx | Tail | Throws
+        | Underscore | Self | TSelf | Where
+        | True | False
             -> true
         | _ -> false
